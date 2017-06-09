98cb954820767f4c66f2ce60aae0820813386688
Sergio Ródenas <serrogo@tedium.es>
2017-06-09 22:17:05 +0200

    Laravel default authentication with Passport

--- config/app.php
+++ config/app.php
@@ -176,7 +176,12 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        //Laravel Passport
+        Laravel\Passport\PassportServiceProvider::class,
+        //Laravel CORS
+        Barryvdh\Cors\ServiceProvider::class,
+        //Laravel Debugbar
+        Barryvdh\Debugbar\ServiceProvider::class,
     ],
 
     /*
@@ -225,7 +230,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
     ],
 
 ];
