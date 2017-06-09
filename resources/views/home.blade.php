eb4de4ab1970847502e4bc7e0e9018859cb36077
Sergio Ródenas <serrogo@tedium.es>
2017-06-09 23:34:03 +0200

    Working on views

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,17 +5,36 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
                 <div class="panel-body">
                     <h1>Recent Reports</h1>
                     <ul class="list-group">
-                        <li class="list-group-item report alert_orange row">
-                            <span class="person col-sm-4">Rob</span>
-                            <span class="report-date col-sm-4">16-may-2017</span>
+                        <li class="list-group-item disabled row">
+                            <span class="person col-sm-4">Name</span>
+                            <span class="location col-sm-3">Location</span>
+                            <span class="report-date col-sm-2">Report Date</span>
+                            <span class="col-sm-2">Alert Type</span>
                         </li>
-                        <li class="list-group-item report alert_red">Fred</li>
-                        <li class="list-group-item report alert_black">James</li>
+                        <li class="list-group-item report alert_orange row">
+                            <span class="person col-sm-4">Samer</span>
+                            <span class="location col-sm-3">Beirut, Lebanon</span>
+                            <span class="report-date col-sm-2">12-dec-2016</span>
+                            <span class="flag col-sm-1"></span>
+                            <span class="col-sm-2"><span class="glyphicon glyphicon-plus-sign"></span>Details</span>
+                        </li>
+                        <li class="list-group-item report alert_red row">
+                            <span class="person col-sm-4">Ozden</span>
+                            <span class="location col-sm-3">Ankara, Turkey</span>
+                            <span class="report-date col-sm-2">16-may-2017</span>
+                            <span class="flag col-sm-1"></span>
+                            <span class="col-sm-2"><span class="glyphicon glyphicon-plus-sign"></span>Details</span>
+                        </li>
+                        <li class="list-group-item report alert_black row">
+                            <span class="person col-sm-4">Johar</span>
+                            <span class="location col-sm-3">Grozny, Russia</span>
+                            <span class="report-date col-sm-2">03-June-2017</span>
+                            <span class="flag col-sm-1"></span>
+                            <span class="col-sm-2"><span class="glyphicon glyphicon-plus-sign"></span>Details</span>
+                        </li>
                     </ul>
                 </div>
             </div>
